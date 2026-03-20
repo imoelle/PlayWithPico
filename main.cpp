b6951708fd2d01d4abc7fbc707bdebfc00176f50
Ingo <73609246+imoelle@users.noreply.github.com>
2026-03-20 07:59:40 +0100

    clean up main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
+#include <stdio.h>
 
-int main()
-{
+#include "lib/hal/gpio/include/GpioPinMode.hpp"
+
+int main() {
     stdio_init_all();
 
-    while (true) {
+    volatile GpioPinMode pinMode;
 
+    while (true) {
+        printf("Testing GPIO Pin Mode Status %d...\n", pinMode.isReady(0x00));
+        sleep_ms(1000);
     }
 }
